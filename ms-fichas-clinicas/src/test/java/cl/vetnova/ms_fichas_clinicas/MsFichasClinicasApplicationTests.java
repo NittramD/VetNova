47755dc0a2bcbd35723f353cfa51e41d6ef9c790
NittramD <CETECOM@COANBH06CW1321.duoc.acad>
2026-05-19 12:34:55 -0400

    configuración inicial microservicios dfs

---
+++ ms-fichas-clinicas/src/test/java/cl/vetnova/ms_fichas_clinicas/MsFichasClinicasApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.vetnova.ms_fichas_clinicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFichasClinicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
